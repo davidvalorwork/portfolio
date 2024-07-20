8eed56c57f9772da7c51592f6c7311f783746d3b
David Valor <davidvalorwork@gmail.com>
2024-07-20 12:16:29 -0400

    Changed names of scripts

---
+++ bin/logs-scanner.sh
@@ -0,0 +1,1 @@
+ssh -i bin/clave-proyecto1.pem ubuntu@18.117.243.194 'sudo docker logs scanner'
--- bin/update-scanner.sh
+++ bin/update-scanner.sh
@@ -1,7 +1,9 @@
+echo "updating scanner making build"
 cd ../Scrappers/scanning-facebook-scrapper/
 docker build -t davidvalorwork/scanner .
 docker push davidvalorwork/scanner
 cd ../../portfolio
+echo "updating scanner in server"
 ssh -i bin/clave-proyecto1.pem ubuntu@18.117.243.194 'sudo docker stop scanner && sudo docker rm scanner'
 ssh -i bin/clave-proyecto1.pem ubuntu@18.117.243.194 'sudo docker pull davidvalorwork/scanner'
 ssh -i bin/clave-proyecto1.pem ubuntu@18.117.243.194 'sudo docker run -d -p 4000:4000 --name scanner davidvalorwork/scanner'
